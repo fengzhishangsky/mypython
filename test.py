9059b926d3d4d0d7bc37a5b6c9edcf07c8b0a5b1
[fengzhishangsky] <[fengzhishangsky@163.com]>
2017-03-03 18:30:10 +0800

    third

--- test.py
+++ test.py
@@ -6,12 +6,27 @@
 #
 #
 #itervalues  直接去value值 eg 求平均值
+# d = { 'Adam': 95, 'Lisa': 85, 'Bart': 59, 'Paul': 74 }
+
+# sum = 0.0
+# for v in d.itervalues():
+#     sum +=v
+# print sum
+# print sum/len(d)
+
+#dict item()
+#
+#
 d = { 'Adam': 95, 'Lisa': 85, 'Bart': 59, 'Paul': 74 }
 
 sum = 0.0
-for v in d.itervalues():
-    sum +=v
-print sum
-print sum/len(d)
+for k, v in d.items():
+    sum = sum + v
+    print k, ":", v
+print 'average', ':', sum/len(d)
 
-#dict
+
+for m, n in d.iteritems():
+    sum = sum + n
+    print m, ":", n
+print 'average', ':', sum/len(d)
