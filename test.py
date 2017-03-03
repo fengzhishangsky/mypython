57d91420b3d44f16c4d2358a40466e98d39fc1b3
fengzhishangsky <fengzhishangsky@163.com>
2017-03-03 17:32:36 +0800

    my first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#这是一个注释的练习
+#
+#
+#
+#
+#itervalues  直接去value值 eg 求平均值
+d = { 'Adam': 95, 'Lisa': 85, 'Bart': 59, 'Paul': 74 }
+
+sum = 0.0
+for v in d.itervalues():
+    sum +=v
+print sum
+print sum/len(d)
