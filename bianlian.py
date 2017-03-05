caa4d94babba67e43db63a618a4c7605a6339f9a
fengzhishangsky <fengzhishangsky>
2017-03-05 14:31:06 +0800

    变量

---
+++ bianlian.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+
+
+#python 列表List
+list1 = ["asdgh","sad"]
+list2 = ['sad','as',1,2,1.2]
+list3 = ["a", "b", "c", "d"];
+
+print ("list[0]"+list1[0])
+#dict
+#
+#
+#Python 元组 tup1  不能修改
+tup1 = ('physics', 'chemistry', 1997, 2000);
+tup2 = (1, 2, 3, 4, 5 );
+tup3 = "a", "b", "c", "d";
