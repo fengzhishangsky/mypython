f3b3f95f6fa02879f3a376855905fc0de4eb4e89
[fengzhishangsky] <[fengzhishangsky@163.com]>
2017-03-06 18:25:08 +0800

    aasd

--- bianlian.py
+++ bianlian.py
@@ -16,4 +16,6 @@ print ("list[0]"+list1[0])
 #Python 元组 tup1  不能修改
 tup1 = ('physics', 'chemistry', 1997, 2000);
 tup2 = (1, 2, 3, 4, 5 );
-tup3 = "a", "b", "c", "d";
+tup3 = ("a", "b", "c", "d");
+#tup1.clear();
+print(tup2);
